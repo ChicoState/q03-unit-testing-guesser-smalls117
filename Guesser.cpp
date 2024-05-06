f8c7dd293b7c684ace5f8116f47c14bc0bd25f2b
jack mattern <jackfm21@yahoo.com>
2024-05-05 23:18:27 -0700

    corrected cpp file

--- Guesser.cpp
+++ Guesser.cpp
@@ -15,7 +15,20 @@ using std::string;
   has 100, the distance is 10.
 */
 unsigned int Guesser::distance(string guess){
-  return 0;
+  unsigned int minLength = (guess.size() < m_secret.size() ? guess.size() : m_secret.size());
+  
+  unsigned int distance = 0;
+  for (unsigned int i = 0; i < minLength; i++) {
+    if(guess[i] != m_secret[i]) {
+      distance++;
+    }
+  }
+  //if distance is greater than the secrets length return the size of the secret
+  if(distance > m_secret.size()) {
+    return m_secret.size();
+  }
+
+  return distance;
 }
 
 /*
@@ -24,8 +37,11 @@ unsigned int Guesser::distance(string guess){
   of any Guesser object and must have a length of 32 characters or less,
   otherwise, it will be truncated at that length.
 */
-Guesser::Guesser(string secret){
-
+Guesser::Guesser(string secret) : m_secret(secret) {
+  m_remaining = 3;
+  //If it's longer than 32 characters, size it down to 32 characters. 
+  m_secret = secret.substr(0, min(secret.length(), size_t(32)));
+  
 }
 
 /*
@@ -40,7 +56,25 @@ Guesser::Guesser(string secret){
   and the secret.
 */
 bool Guesser::match(string guess){
-  return true;
+  int guesses = remaining();
+
+  //if they guessed correctly
+  if (distance(guess) == 0) {
+    m_remaining = 3;
+    return true;
+  }
+
+  //if they are out of guesses
+  if (guesses == 0) {
+    return false;
+  }
+
+  //if there guess is off by more than 2
+  if (distance(guess) > 2) {
+    return false;
+  }
+
+  return false;
 }
 
 /*
@@ -51,7 +85,13 @@ bool Guesser::match(string guess){
   reset to three (3).
 */
 unsigned int Guesser::remaining(){
-  int guesses = 3;
-  return 0;
+
+  if(m_remaining == 0) {
+    return m_remaining;
+  }
+
+  m_remaining--;
+  
+  return m_remaining;
 }
 
